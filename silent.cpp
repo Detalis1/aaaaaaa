233b19474b30ec70d946caffc008337f9cb59941
Detalis1 <102513511+Detalis1@users.noreply.github.com>
2023-09-11 18:42:27 +0300

    Update silent.cpp

--- silent.cpp
+++ silent.cpp
@@ -4,19 +4,6 @@ void __fastcall HOOK_CreateMove(DWORD* ecx, DWORD* edx, int sequence_number, flo
 	DWORD yaw = 0, pitch = 0;
 	yaw = GetMem<DWORD>(client_dll + offsets::my_angle_x);
 	pitch = GetMem<DWORD>(client_dll + offsets::my_angle_y);
-	if(GetAsyncKeyState(VK_NUMPAD9) && false)
-	{
-		ye = true;
-		yaw = GetMem<DWORD>(client_dll + offsets::my_angle_x);
-		pitch = GetMem<DWORD>(client_dll + offsets::my_angle_y);
-		SetViewAngles(0.0f, -89.0f);
-	}
-
-
-	if (GetAsyncKeyState(VK_NUMPAD6) && false)
-	{
-		return;
-	}
 
 	if (functions.silent_aimbot && (GetAsyncKeyState(VK_LBUTTON) & 1))
 	{
